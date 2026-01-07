0daa9d723f85e292d900f3fcbce5a01103d7b4c3
Ovoderoye Allen <98387608+allenovozy@users.noreply.github.com>
2026-01-07 21:35:27 +0100

    Update investment-summary.jsx

--- components/investment-summary.jsx
+++ components/investment-summary.jsx
@@ -45,7 +45,7 @@ export default function InvestmentSummary({ userId }) {
       return
     }
 
-    fetch("http://185.27.134.59/server/Api/investments.php", {
+    fetch("https://185.27.134.59/server/Api/investments.php", {
       method: "GET",
       credentials: "include",
     })
