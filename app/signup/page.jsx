4268044e73aa89c68a07ed4ac58b32a97fd9e67f
Ovoderoye Allen <98387608+allenovozy@users.noreply.github.com>
2026-01-07 20:42:34 +0100

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -39,7 +39,7 @@ export default function SignupPage() {
     }
 
     try {
-      const res = await fetch("http://192.168.1.150/server/Api/signup.php", {
+      const res = await fetch("http://185.27.134.59/server/Api/signup.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
